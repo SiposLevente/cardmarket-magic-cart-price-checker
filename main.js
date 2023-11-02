d1248f0f635fe2de7baf49e4d9d570f24e7f5950
SiposLevente <sipi.levente@gmail.com>
2023-11-02 23:18:37 +0100

    Added button to check prices

--- main.js
+++ main.js
@@ -9,30 +9,31 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-(function () {
-    'use strict';
-    const shipments = document.getElementById("shipments-col").getElementsByClassName("table table-sm article-table mb-1 table-striped product-table");
-    function displayPrice(collection, name, htmlElement, callback) {
-        GM_xmlhttpRequest({
-            withCredentials: true,
-            method: "GET",
-            url: "https://www.cardmarket.com/en/Magic/Products/Singles/" + collection + "/" + name + "?language=1&minCondition=3",
-            onload: function (response) {
-                try {
-                    const doc = new DOMParser().parseFromString(response.responseText, 'text/html');
-                    const priceArray = doc.getElementById("table").getElementsByClassName("table-body")[0];
-                    const pricesToCheck = priceArray.children.length < 5 ? priceArray.children.length : 5;
-                    const lowestPrice = priceArray.children[0].getElementsByClassName("color-primary small text-end text-nowrap fw-bold")[1].innerHTML;
-                    const highestPrice = priceArray.children[pricesToCheck].getElementsByClassName("color-primary small text-end text-nowrap fw-bold")[1].innerHTML;
-                    htmlElement.getElementsByClassName("name text-start d-none d-md-table-cell")[0].children[0].innerHTML += "<br>( " + lowestPrice + " - " + highestPrice + " )";
-                    callback(true);
-                } catch (e) {
-                    callback(false);
-                }
+function displayPrice(collection, name, htmlElement, callback) {
+    GM_xmlhttpRequest({
+        withCredentials: true,
+        method: "GET",
+        url: "https://www.cardmarket.com/en/Magic/Products/Singles/" + collection + "/" + name + "?language=1&minCondition=3",
+        onload: function (response) {
+            try {
+                const doc = new DOMParser().parseFromString(response.responseText, 'text/html');
+                const priceArray = doc.getElementById("table").getElementsByClassName("table-body")[0];
+                const pricesToCheck = priceArray.children.length < 5 ? priceArray.children.length : 5;
+                const lowestPrice = priceArray.children[0].getElementsByClassName("color-primary small text-end text-nowrap fw-bold")[1].innerHTML;
+                const highestPrice = priceArray.children[pricesToCheck].getElementsByClassName("color-primary small text-end text-nowrap fw-bold")[1].innerHTML;
+                const constCardNameElement = htmlElement.getElementsByClassName("name text-start d-none d-md-table-cell")[0].children[0];
+                constCardNameElement.innerHTML = constCardNameElement.innerHTML.replace(/<br>\(\s\d+(?:,\d+)?\s€\s-\s\d+(?:,\d+)?\s€\s\)/g, '');
+                constCardNameElement.innerHTML += "<br>( " + lowestPrice + " - " + highestPrice + " )";
+                callback(true);
+            } catch (e) {
+                callback(false);
             }
-        });
-    }
+        }
+    });
+}
 
+function getPrices() {
+    const shipments = document.getElementById("shipments-col").getElementsByClassName("table table-sm article-table mb-1 table-striped product-table");
     for (let indexShiplents = 0; indexShiplents < shipments.length; indexShiplents++) {
         let rows = shipments[indexShiplents].getElementsByTagName("tbody")[0].getElementsByTagName("tr");
         for (let indexRows = 0; indexRows < rows.length; indexRows++) {
@@ -40,10 +41,19 @@
             const cardName = rows[indexRows].getAttribute("data-name").replace(/\s+/g, '-').replace(/,+/g, '').replace(/\.+/g, '').replace(/\(|\)/g, '');
             displayPrice(collection, cardName.replace(/'+/g, ''), rows[indexRows], function (wasExecSuccessful) {
                 if (!wasExecSuccessful) {
-                    displayPrice(collection, cardName.replace(/'+/g, '-'), rows[indexRows], function (_unusedVar) { console.log("Unable to get " + cardName + " from collection " + collection) });
+                    displayPrice(collection, cardName.replace(/'+/g, '-'), rows[indexRows], function (_unusedVar) { });
                 }
             });
         }
     }
+}
+
+(function () {
+    'use strict';
+    let button = document.createElement("button");
+    button.innerHTML = "Get Prices";
+    button.classList += "btn btn-primary btn-sm btn-outline-primary";
+    button.addEventListener("click", getPrices, false)
+    document.getElementsByClassName("card-body d-flex flex-column")[1].insertBefore(button, document.getElementsByClassName("card-body d-flex flex-column")[1].children[2]);
 })();
 
